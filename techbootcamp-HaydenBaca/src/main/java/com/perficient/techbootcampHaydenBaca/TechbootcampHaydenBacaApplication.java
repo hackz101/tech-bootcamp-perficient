0f599fd06255b31df25eaa8054f4ac81aff600bf
Hayden Baca <therealprogrammer4444@gmail.com>
2022-01-17 14:52:35 -0600

    Spring Init

---
+++ techbootcamp-HaydenBaca/src/main/java/com/perficient/techbootcampHaydenBaca/TechbootcampHaydenBacaApplication.java
@@ -0,0 +1,13 @@
+package com.perficient.techbootcampHaydenBaca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbootcampHaydenBacaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechbootcampHaydenBacaApplication.class, args);
+	}
+
+}
